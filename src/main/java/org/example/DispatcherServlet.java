8bfd46078f1b467ab37e9c8bc89f2e8f2c1929c7
naocya@naver.com <naocya@naver.com>
2022-12-10 20:07:39 +0900

    servlet library 추가

--- src/main/java/org/example/DispatcherServlet.java
+++ src/main/java/org/example/DispatcherServlet.java
@@ -1,4 +1,21 @@
 package org.example;
 
-public class DispatcherServlet  {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class DispatcherServlet  extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp
+    ) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
