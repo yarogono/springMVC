f87794ba547b4873f5693d8b39026112e3bafb3c
naocya@naver.com <naocya@naver.com>
2022-12-15 15:45:31 +0900

    DispatcherServlet, HandlerMapping, ViewResolver 코드 작성

--- src/main/java/org/example/DispatcherServlet.java
+++ src/main/java/org/example/DispatcherServlet.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,16 +8,41 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class DispatcherServlet  extends HttpServlet {
+    private HandlerMapping handlerMapping;
+    private ViewResolver viewResolver;
 
     @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
+    public void init() throws ServletException {
+        handlerMapping = new HandlerMapping();
+        viewResolver = new ViewResolver();
+        viewResolver.setPrefix("./");
+        viewResolver.setSuffix(".jsp");
     }
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        service(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        service(req, resp);
+    }
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.service(req, resp);
-    }
+        String uri = req.getRequestURI();
+        String path = uri.substring(uri.lastIndexOf("/"));
 
+        Controller controller = handlerMapping.getController(path);
+
+        String viewName = controller.handleRequest(req, resp);
+
+        String view = null;
+        if (!viewName.contains(".do")) {
+            view = viewResolver.getView(viewName);
+        } else {
+            view = viewName;
+        }
+    }
 }
--- src/main/java/org/example/HandlerMapping.java
+++ src/main/java/org/example/HandlerMapping.java
@@ -1,4 +1,19 @@
 package org.example;
 
-public interface HandlerMapping {
+import org.example.controller.UserController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HandlerMapping {
+    private Map<String, Controller> mappings;
+
+    public HandlerMapping() {
+        mappings = new HashMap<String, Controller>();
+        mappings.put("/user.do", new UserController());
+    }
+
+    public Controller getController(String path) {
+        return mappings.get(path);
+    }
 }
