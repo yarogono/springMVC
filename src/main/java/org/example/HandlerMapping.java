f04c5201124c661132dcf1340e7a63a04c9c225a
naocya@naver.com <naocya@naver.com>
2022-12-15 16:15:22 +0900

    HandlerMapping 설명 추가

--- src/main/java/org/example/HandlerMapping.java
+++ src/main/java/org/example/HandlerMapping.java
@@ -5,8 +5,10 @@ import org.example.controller.UserController;
 import java.util.HashMap;
 import java.util.Map;
 
+// Dispatcher Servlet으로 들어온 모든 요청을 각각의 Controller로 위임하는 역할
 public class HandlerMapping {
     private Map<String, Controller> mappings;
+
 
     public HandlerMapping() {
         mappings = new HashMap<String, Controller>();
