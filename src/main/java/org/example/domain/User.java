df3d4447a72f11b92b0570f12952d62132c560ca
naocya@naver.com <naocya@naver.com>
2022-12-19 12:07:39 +0900

    도메인 추가

---
+++ src/main/java/org/example/domain/User.java
@@ -0,0 +1,25 @@
+package org.example.domain;
+
+public class User {
+
+    private final long id;
+    private final String username;
+    private final String password;
+    private final String email;
+
+    public User(long id, String username, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
